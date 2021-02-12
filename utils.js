f78167c2b1a695b7820fb2fcc228a67018a7e7c1
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-12 15:38:33 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ function normalizeName(name) {
 function normalizeInterpolate(n, start1, end1, start2, end2) {
   return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
 }
-// new Date => "9th February"
+// new Date => "February 12"
 function formatDate(date) { 
     return date.toLocaleDateString(undefined, { day: 'numeric', month: 'long' }); 
 }
