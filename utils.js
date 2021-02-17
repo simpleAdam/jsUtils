01bed281b3d720c3f606bbd03e22adadb9be0220
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-17 15:58:34 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,6 +3,11 @@ function normalizeName(name) {
     return name.replace(/\s+/g, '-').toLowerCase();
 }
 
+// "<p></p>" => "&lt;p&gt;&lt;/p&gt;"
+const cleanseString = function(string) {
+  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+};
+
 // (3,1,5,10,50) => 30
 function normalizeInterpolate(n, start1, end1, start2, end2) {
   return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
